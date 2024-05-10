158a99075c722c83d100b45a59c7dac5d3d1b2e6
henriqueguanais <henriquecorneaudr@gmail.com>
2024-05-10 13:50:52 -0300

    alteracao nos codigos contador

--- cofre_digital/componentes/contador_senha.vhd
+++ cofre_digital/componentes/contador_senha.vhd
@@ -3,33 +3,28 @@ use ieee.std_logic_1164.all;
 
 entity cont_senha is
     port (s_cont: in  std_logic_vector(2 downto 0);
-	  add_cont, clk_cont: in std_logic;
+	      add_cont, clk_cont: in std_logic;
           cont_end: out std_logic);
 end cont_senha;
 
 architecture log of cont_senha is
 
-component ffd is
-	port (ck, clr, set, d : in  std_logic;
-                       q : out std_logic);
+component ffjk is
+    port (ck, clr, set, j, k : in  std_logic;
+                            q : out std_logic);
 end component;
 
-signal D_FFD, clr_cont, D_and_Q, D_en_0, D_FFD_0, set_FFD_1: std_logic:= '0';
-signal Q_FFD: std_logic_vector(1 downto 0):= (OTHERS => '0');
+signal and_1, clr_FFJK: std_logic;
+signal Q_FFJK: std_logic_vector(1 downto 0);
 
 begin
-	
-    D_FFD <= add_cont and not s_cont(2) and s_cont(1) and not s_cont(0);
-    clr_cont <= not(add_cont and not s_cont(2) and not s_cont(1) and not s_cont(0));
 
-    D_and_Q <= D_FFD and not Q_FFD(0);
-    D_en_0 <= not Q_FFD(0) or not Q_FFD(1);
-    D_FFD_0 <= (not D_en_0 and Q_FFD(0)) or (D_en_0 and D_and_Q);
-    set_FFD_1 <= (Q_FFD(1));
+    and_1 <= not s_cont(0) and s_cont(1) and not s_cont(2);
+    clr_FFJK <= and_1 and add_cont;
 
-    Q0: ffd port map(clk_cont, clr_cont, '1', D_and_Q, Q_FFD(0));
-    Q1: ffd port map(clk_cont, clr_cont, set_FFD_1, Q_FFD(0), Q_FFD(1));
-    
-    cont_end <= '1' and Q_FFD(1) and Q_FFD(0);
+    Q0: ffjk port map(clk_cont, clr_FFJK, '1', '1', '1', Q_FFJK(0));
+    Q1: ffjk port map(clk_cont, clr_FFJK, '1', Q_FFJK(0), Q_FFJK(0), Q_FFJK(1));
+
+    cont_end <= Q_FFJK(0) and Q_FFJK(1);
 
 end log;
